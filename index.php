0270bd370a4108121163dc851beddcf379a3137c
afdhaa <ihsan.r28@gmail.com>
2016-08-05 20:42:49 +0700

    apa2an ini

--- index.php
+++ index.php
@@ -4,4 +4,4 @@
 --ini bagian dari jangkrik--
 <?php echo "jangkrik bos"; ?>
 --ini bagian dari tambahan jangkrik--
-<?php echo "jangkrik tambahannya"; ?>
+<?php echo "jangkrik tambahannya 2 kali"; ?>
